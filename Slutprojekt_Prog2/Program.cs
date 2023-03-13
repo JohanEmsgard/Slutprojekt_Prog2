402130e6292de1a70d80090c56ed705f5abe7c9f
Johanemsgard <emsgard.johan@elev.ga.ntig.se>
2023-03-13 08:59:53 +0100

    fasgjfdgj

--- Slutprojekt_Prog2/Program.cs
+++ Slutprojekt_Prog2/Program.cs
@@ -5,16 +5,20 @@ Raylib.SetTargetFPS(60);
 
 Sonic sonic = new Sonic(); 
 
-Texture2D background = Raylib.LoadTexture("GreenHillZone.png");
+//Texture2D background = Raylib.LoadTexture("GreenHillZone.png");
 
 while(!Raylib.WindowShouldClose())
 {
+    //logik
+    sonic.Update();
+
+
     //Grafik
     Raylib.BeginDrawing();
     Raylib.ClearBackground(Color.WHITE);
-    Raylib.DrawTexture(background, 0,0, Color.WHITE);
+    //Raylib.DrawTexture(background, 0,0, Color.WHITE);
     sonic.Draw();
-    
+
 
 
     Raylib.EndDrawing();
--- Slutprojekt_Prog2/Sonic.cs
+++ Slutprojekt_Prog2/Sonic.cs
@@ -1,7 +1,5 @@
 using System;
 
-namespace Slutprojekt_Prog2;
-
 public class Sonic
 {
     //postion 
@@ -10,5 +8,23 @@ public class Sonic
     
     public float speed = 5f;
 
-    
+    public Sonic()
+    {
+        sprite = Raylib.LoadTexture("./Sonic.png");
+        rect = new Rectangle(0,0,sprite.width, sprite.height);
+    }
+
+    public void Update()
+    {
+        if (Raylib.IsKeyDown(KeyboardKey.KEY_RIGHT))
+        {
+            rect.x += speed;
+        }
+    }
+
+
+    public void Draw()
+    {
+        Raylib.DrawTexture(sprite, (int) rect.x, (int)rect.y,Color.WHITE);
+    }
 }
