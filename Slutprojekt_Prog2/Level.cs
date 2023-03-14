a327e7a15039c78b782fd20dbad4e9d7738c3bed
Johanemsgard <emsgard.johan@elev.ga.ntig.se>
2023-03-14 13:20:35 +0100

    jkhdh

--- Slutprojekt_Prog2/Level.cs
+++ Slutprojekt_Prog2/Level.cs
@@ -5,7 +5,19 @@ public class Level
     Random Generator = new Random();
     List<Rectangle> platforms = new List<Rectangle>();
 
-    
+
+    int [,] level = new int[5,8];
+
+    public void openlevel()
+    {
+        level = new int[5,8]{
+            {0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0},
+        };
+    }
 
 
 }
