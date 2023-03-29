a010248c2d766efdad04ed7abbe2a5063d684299
Johanemsgard <emsgard.johan@elev.ga.ntig.se>
2023-03-29 12:14:56 +0200

    xvhjbxcbvxhukckhudbjkfb

--- Slutprojekt_Prog2/SuperSonic.cs
+++ Slutprojekt_Prog2/SuperSonic.cs
@@ -8,6 +8,7 @@ public class SuperSonic : Sonic
         idle = Raylib.LoadTexture("SuperSonic.png");
         run = Raylib.LoadTexture("SupersonicFly.png");
         runLeft = Raylib.LoadTexture("SupersonicFlyLeft.png");
+        spinSprite = Raylib.LoadTexture("Supersonicsindash.png");
     }
         
 }
